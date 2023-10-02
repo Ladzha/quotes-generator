6f3a626333e9d970cae4a5ce7b27e4567192a312
Alla <allanudelman@gmail.com>
2023-10-02 13:36:00 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Quote from './components/Quote.js'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Quote/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,44 @@
+import quotes from './quotes'
+import colors from './colors'
+import {useState, useEffect} from 'react'
+
+
+const Quote = ()=>{
+    const [num, setNum] = useState(0)
+
+    console.log(quotes.length);
+
+    const random = ((min, max)=>{
+        let random;
+        do {
+           random = Math.floor(Math.random()*(max-min+1));
+        }while(
+            random===num
+        )
+        return random;
+    })
+    
+    const ranIndex =random(0, quotes.length);
+
+    const handleClick= ()=>{
+        setNum(ranIndex)
+    }
+
+    return(
+        <div className="container" >
+            {quotes[num].quote?
+                (
+                    <div>
+                    <h2 className="quote">"{quotes[num].quote}"</h2>
+                    <p className="author">{quotes[num].author}</p>
+                    </div>
+                ) : (<h2 className="quote">Try again</h2>
+                )}
+            
+            <button className="button" onClick={handleClick}>Generate</button>
+        </div>
+    )
+
+}
+
+export default Quote
